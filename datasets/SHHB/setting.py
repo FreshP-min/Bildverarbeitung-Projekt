a6438119dc8121a19451ae89f50f433fa4e463e7
Philipp Langsteiner <langstei@cgcontact.cs.uni-tuebingen.de>
2024-02-25 14:50:12 +0100

    new models

--- datasets/SHHB/setting.py
+++ datasets/SHHB/setting.py
@@ -1,7 +1,7 @@
 class ConfigSHHB:
     def __init__(self):
         self.STD_SIZE = (768, 1024)
-        self.TRAIN_SIZE = (768, 1024)# (576, 768)
+        self.TRAIN_SIZE = (576, 768)
         self.DATA_PATH = '/graphics/scratch2/students/langstei/ProcessedData/shanghaitech_part_B/'
         self.MEAN_STD = (
             [0.452016860247, 0.447249650955, 0.431981861591],
@@ -10,7 +10,7 @@ class ConfigSHHB:
         self.LABEL_FACTOR = 1
         self.LOG_PARA = 100.
         self.RESUME_MODEL = '../../models/SCC_Model/Res50.py'  # model path
-        self.TRAIN_BATCH_SIZE = 4  # imgs
+        self.TRAIN_BATCH_SIZE = 8  # imgs
         self.VAL_BATCH_SIZE = 1
 
 # Create an instance of the ConfigSHHB class
